4334e423f78893c85ff1ffd3ff5cfc54dbadfa10
hank <hank@jointsecurityarea.org>
2018-04-16 01:18:15 -0400

    Moving to better sched, small changes

--- bgescrape.py
+++ bgescrape.py
@@ -1,13 +1,5 @@
-import sched
-import datetime
-import re
-import io
-import csv
+import re, io, csv, os, glob, time, configparser
 from zipfile import ZipFile
-import os
-import glob
-import time
-import configparser
 from selenium.webdriver import Firefox, FirefoxProfile
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
@@ -15,14 +7,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from apscheduler.schedulers.background import BackgroundScheduler
 
-def calculateStartAndEndDate():
-    tod = datetime.datetime.now()
-    d = datetime.timedelta(days=3)
-    start_date = (tod - d).strftime("%m%d%Y")
-    end_date = tod.strftime("%m%d%Y")
-    print("Pulling {} through {}".format(start_date, end_date))
-    return (start_date, end_date)
 
 def processZips(output_dir):
     zips = glob.glob(os.path.join(output_dir, "*.zip"))
@@ -39,6 +25,8 @@ def processZips(output_dir):
                             if (len(row) == 8 and
                                re.search(r'Electric|Gas', row[0], re.I) is not None):
                                 print(" | ".join(row))
+        # Remove the zip
+        os.unlink(os.path.join(output_dir, i))
 
 def buildDriver(output_dir):
     profile = FirefoxProfile()
@@ -70,7 +58,7 @@ def login(driver, username, password):
     print("Logging in...")
     actions.perform()
 
-def green_buttonize(driver, start_date, end_date):
+def green_buttonize(driver):
     elem = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
             (By.XPATH, "//div[@id='widget-usage-export']/div/div[2]/form/div/div/div/div[3]/div/label"),
         )
@@ -79,34 +67,15 @@ def green_buttonize(driver, start_date, end_date):
     driver.execute_script("arguments[0].scrollIntoView(true);", elem)
     elem.click()
 
-    # The sequence here sucks because of the stupid date picker.
-    actions = ActionChains(driver)
-    actions.send_keys(Keys.TAB)
-    actions.perform()
-    print("Entering Start Date")
-    elem = driver.find_element_by_id("date-picker-from")
-    slow_send_keys(elem, start_date[:2])
-    actions = ActionChains(driver)
-    actions.send_keys(Keys.TAB)
-    actions.perform()
-    slow_send_keys(elem, start_date[2:])
-    actions = ActionChains(driver)
-    actions.send_keys(Keys.TAB)
-    actions.perform()
-    time.sleep(0.5)
-    print("Entering End Date")
-    elem = driver.find_element_by_id("date-picker-to")
-    slow_send_keys(elem, end_date)
     elem = driver.find_element_by_css_selector("input.button.primary")
     elem.click()
 
 def slow_send_keys(elem, keys):
     for c in keys:
         elem.send_keys(c)
-        time.sleep(0.3)
+        time.sleep(1)
 
-def initiateRequest(driver, username, password, tmp_output_dir, s):
-    start_date, end_date = calculateStartAndEndDate()
+def initiateRequest(driver, username, password, tmp_output_dir):
     try:
         login(driver, username, password)
 
@@ -136,7 +105,7 @@ def initiateRequest(driver, username, password, tmp_output_dir, s):
         driver.execute_script("arguments[0].scrollIntoView(true);", elem)
         elem.click()
 
-        green_buttonize(driver, start_date, end_date)
+        green_buttonize(driver)
 
         print("Waiting for download to complete")
         time.sleep(5)
@@ -144,11 +113,6 @@ def initiateRequest(driver, username, password, tmp_output_dir, s):
         processZips(tmp_output_dir)
     except Exception as e:
         print("Exception encountered: {}".format(e))
-    # Schedule the next one
-    d = datetime.timedelta(minutes=5)
-    s.enter(d.total_seconds(), 1, initiateRequest,
-            argument=(driver, username, password, tmp_output_dir, s))
-    s.run()
 
 def run():
     config = configparser.ConfigParser()
@@ -167,8 +131,21 @@ def run():
 
     driver = buildDriver(tmp_output_dir)
 
-    s = sched.scheduler()
-    initiateRequest(driver, username, password, tmp_output_dir, s)
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(initiateRequest, 'interval', minutes=2,
+        args=(driver, username, password, tmp_output_dir))
+
+    # Run the first job
+    initiateRequest(driver, username, password, tmp_output_dir)
+
+    print("Starting scheduler")
+    scheduler.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except:
+        scheduler.shutdown(wait=False)
 
     print("Exiting driver")
     driver.quit()
